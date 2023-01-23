4b1910581ad07f1cfe543aa7059a758c3974241e
Vitap7 <aylsn123@qq.com>
2023-01-23 21:08:04 +0800

    refactor: use %20 to replace whitespace and fetch

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
-import data from "./data.json" assert { type: "json" };
+fetch("data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const myBars = document.querySelectorAll(".bar");
 
-const myBars = document.querySelectorAll(".bar");
+    const todayIndex = (new Date().getDay() + 6) % 7;
 
-const todayIndex = (new Date().getDay() + 6) % 7;
+    myBars.forEach((bar, number) => {
+      if (number === todayIndex) {
+        bar.classList.add("today");
+      }
+      bar.dataset.id = number;
+      bar.dataset.amount = data[number].amount;
+      bar.setAttribute("style", `--amount: ${data[number].amount}`);
+    });
 
-myBars.forEach((bar, number) => {
-  if (number === todayIndex) {
-    bar.classList.add("today");
-  }
-  bar.dataset.id = number;
-  bar.dataset.amount = data[number].amount;
-  bar.setAttribute("style", `--amount: ${data[number].amount}`);
-});
+    const showAmount = (e) => {
+      const dataset = e.target.dataset;
+      myBars[dataset.id].insertAdjacentHTML(
+        "beforebegin",
+        `<div class="amount">$${dataset.amount}</div>`
+      );
+    };
 
-const showAmount = (e) => {
-  const dataset = e.target.dataset;
-  myBars[dataset.id].insertAdjacentHTML(
-    "beforebegin",
-    `<div class="amount">$${dataset.amount}</div>`
-  );
-};
+    const hideAmount = (e) => {
+      e.target.previousSibling.remove();
+    };
 
-const hideAmount = (e) => {
-  e.target.previousSibling.remove();
-};
-
-myBars.forEach((myBar) => {
-  myBar.addEventListener("mouseover", showAmount);
-});
-myBars.forEach((myBar) => {
-  myBar.addEventListener("mouseleave", hideAmount);
-});
+    myBars.forEach((myBar) => {
+      myBar.addEventListener("mouseover", showAmount);
+    });
+    myBars.forEach((myBar) => {
+      myBar.addEventListener("mouseleave", hideAmount);
+    });
+  })
+  .catch((error) => console.log(error));
